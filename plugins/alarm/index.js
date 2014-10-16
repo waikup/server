8ac79a0c30e32d194c837aeb049b5b83e90b8372
alexperezpaya <alejandro@perezpaya.net>
2014-10-17 01:27:50 +0200

    TTS Working

--- plugins/alarm/index.js
+++ plugins/alarm/index.js
@@ -1,9 +1,9 @@
 //var moment = require("moment")
-var request = require('request');
-//var tts = require(__dirname + '/../../core/helpers/tts');
+//var request = require('request');
+
+var tts = require(process.cwd() + '/tts');
 
 module.exports = exports = function (stream, attr, cb){
-    console.log(attr);
-    request('http://uhmp3.com/user-mp3-to/8_wiggle-ft-snoop-dogg-jason-derulo.mp3').pipe(stream);
-    cb();
+    //request('http://uhmp3.com/user-mp3-to/8_wiggle-ft-snoop-dogg-jason-derulo.mp3').pipe(stream);
+    tts(attr.text || 'Good morning', attr.lang || 'en', stream);
 }
