56a32626cacdb9bb7eee43a9e9196492f5380f3a
Luis Iván <me@luisivan.net>
2014-10-16 02:44:12 +0200

    Jorge and Alex are srsly drunk

---
+++ plugins/soundcloud/config/script.js
@@ -0,0 +1,38 @@
+SC.initialize({
+    client_id: "5a8edbed865ed2b31acf4d9720696e7f"
+})
+
+$(document).on('ready', function() {
+	var id = Plugin.getConfig()['attr']['id']
+	if (!id) return
+
+	SC.get("/tracks/"+id+".json", null, function(track) {
+		appendLi(track)
+	})
+})
+
+function search(query, cb) {
+	SC.get("/tracks", {q: query, limit: 5}, cb)
+}
+
+function appendLi(track) {
+	var a = $('<a>').text(track.title).append('<img src="'+track.artwork_url+'">'),
+		li = $('<li>').data('id', track.id).append(a)
+
+	$('ul').append(li)
+}
+
+$('[role=main]').on('keypress', 'input', function(e) {
+	if (e.which != 13) return
+
+	$('ul').html('')
+	search($(this).val(), function(tracks) {
+		tracks.forEach(function(track) {
+			appendLi(track)
+		})
+	})
+})
+
+$('ul').on('tap', 'li', function() {
+	Plugin.sendConfig({'id': parseInt($(this).data('id'))})
+})
