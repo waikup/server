80745bb6c2517676442c13dc8c47e5d05ffb7750
alexperezpaya <alejandro@perezpaya.net>
2014-10-17 10:34:31 +0200

    Tests

--- plugins/soundcloud/index.js
+++ plugins/soundcloud/index.js
@@ -2,8 +2,8 @@ var	request = require('request')
 
 module.exports = function (stream, attr, callback) {
 
-	attr.id = attr.id || 165242233;
+	attr.songId = attr.songId || 165242233;
 
-	request.get('http://api.soundcloud.com/tracks/' + attr.id + '/stream?client_id=5a8edbed865ed2b31acf4d9720696e7f').pipe(stream);
+	request.get('http://api.soundcloud.com/tracks/' + attr.songId + '/stream?client_id=5a8edbed865ed2b31acf4d9720696e7f').pipe(stream);
 	
 }
